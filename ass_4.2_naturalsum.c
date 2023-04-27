2eececd288df67c1ab0daeae13f32d4ab96a838b
shristi-lodha <shristilodha84@gmail.com>
2023-04-27 14:29:46 +0530

    finding natural sum using formula

---
+++ ass_4.2_naturalsum.c
@@ -0,0 +1,12 @@
+//finding using formula -
+
+#include<stdio.h>
+int main(){
+    int n;
+printf("enter the number :");
+scanf("%d",&n);
+int sum =(n*(n+1))/2;
+printf("the sum of first %d natural number is :%d",n,sum);
+return 0;
+}
+
